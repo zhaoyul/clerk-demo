d283c4a220f06918da20d0b8e6fca46c1e22d458
kevin li <12980829@qq.com>
2025-02-19 09:56:49 +0800

    [doc] 翻译为中文

--- notebooks/data_science.clj
+++ notebooks/data_science.clj
@@ -1,5 +1,3 @@
-;; # A small data science example 🔢
-^{:nextjournal.clerk/visibility {:code :hide}}
 (ns data-science
   (:require [clojure.set :refer [join rename-keys project]]
             [clojure.string :as str]
@@ -11,31 +9,28 @@
             [next.jdbc.result-set :as rs]
             [nextjournal.clerk :as clerk]))
 
-;; # Exploring the world in data
+;; # 一个小型数据科学示例 🔢
+^{:nextjournal.clerk/visibility {:code :hide}}
 
-;; One of the challenges in real data science is getting data from
-;; different sources in many different formats. In this notebook, we
-;; will explore some facts about the world using data taken from a TSV
-;; file, an Excel spreadsheet, and a database query.
+;; # 在数据中探索世界
 
-;; ## Life expectancy
+;; 现实数据科学中的一个挑战是从不同的来源以许多不同的格式获取数据。
+;; 在本notebook中，我们将使用从TSV文件、Excel电子表格和数据库查询中获取的数据，
+;; 探索一些关于世界的事实。
 
-;; First, we'll read in a TSV file containing the most recent CIA
-;; World Factbook data using the
-;; [meta-csv](https://github.com/ngrunwald/meta-csv) library.
+;; ## 预期寿命
+
+;; 首先，我们将使用 [meta-csv](https://github.com/ngrunwald/meta-csv) 库
+;; 读取包含最新 CIA World Factbook 数据的 TSV 文件。
 (def cia-factbook
   (csv/read-csv "./datasets/cia-factbook.tsv"))
 
-;; Expanding the results in the data viewer tells us that there are
-;; some `nil` values in columns of interest, and that our TSV importer
-;; was thrown off by this fact and so didn't convert the numerical
-;; columns to number types.
+;; 在数据查看器中展开结果告诉我们，感兴趣的列中存在一些 `nil` 值，
+;; 并且我们的 TSV 导入器因此被抛出异常，因此没有将数值列转换为数字类型。
 
-;; We're going to post process this table a bit with ordinary Clojure
-;; sequence functions to filter out rows that have `nil`s for our
-;; columns of interest, select those rows, convert strings to numbers,
-;; and — because we're Clojurists — convert keys to keywords.
-
+;; 我们将使用普通的 Clojure 序列函数对这个表进行一些后处理，
+;; 以过滤掉在我们的感兴趣的列中具有 `nil` 值的行，选择这些行，
+;; 将字符串转换为数字，并且——因为我们是 Clojurist——将键转换为关键字。
 (def life-expectancy
   (->> cia-factbook
        (remove #(some nil? (map (partial get %) ["Country" "GDP/cap" "Life expectancy"])))
@@ -43,16 +38,11 @@
                          :gdp (read-string (get % "GDP/cap"))
                          :life-expectancy (read-string (get % "Life expectancy"))))))
 
-;; Things look pretty good in the data structure browser, but it would
-;; be easier to get an overview in tabular form. Luckily, Clerk's
-;; built in table viewer is able to infer how to handle all of the
-;; most common configurations of rows and columns automatically.
-
+;; 在数据结构浏览器中看起来相当不错，但是以表格形式获得概览会更容易。
+;; 幸运的是，Clerk 的内置表格查看器能够推断出如何自动处理所有最常见的行和列配置。
 (clerk/table life-expectancy)
 
-;; We can also graph the data to see if there are any visible
-;; correlation between our two variables of interest, GDP per capita
-;; and life expectancy.
+;; 我们还可以绘制数据图，以查看我们的两个感兴趣的变量（人均 GDP 和预期寿命）之间是否存在任何可见的相关性。
 (clerk/vl
  {:data {:values life-expectancy}
   :width 700
@@ -64,49 +54,39 @@
                  :type :quantitative}
              :tooltip {:field :country}}})
 
-;; Unsurprisingly, it seems that living in an extremely poor country
-;; has negative consequences for life expectancy. On the other hand,
-;; it looks like things start to flatten out once GDP/capita goes
-;; above $10-15k/year. Some other interesting patterns also emerge:
-;; Singapore and Japan have similar life expectancies, despite the
-;; former's GDP being twice the latter's, and Qatar — the richest
-;; nation in the dataset by GDP/capita — has similar average life
-;; expectancy as the Dominican Republic.
+;; 毫不奇怪，生活在一个极其贫穷的国家对预期寿命有负面影响。
+;; 另一方面，看起来一旦人均 GDP 超过每年 10-15k 美元，情况就开始趋于平缓。
+;; 还出现了一些其他有趣的模式：新加坡和日本的预期寿命相似，
+;; 尽管前者的 GDP 是后者的两倍，而卡塔尔——数据集中人均 GDP 最富裕的国家——
+;; 与多米尼加共和国的平均预期寿命相似。
 
-;; ## Inequality
+;; ## 不平等
 
-;; Now, let's try the same experiment using information from a
-;; spreadsheet containing the GINI coefficient — a widely used measure
-;; of income inequality — for each country. We're going to use a
-;; library called [Docjure](https://github.com/mjul/docjure) that
-;; provides access to Microsoft Office file formats.
+;; 现在，让我们尝试使用包含每个国家/地区的 GINI 系数（一种广泛使用的收入不平等指标）的电子表格中的信息进行相同的实验。
+;; 我们将使用一个名为 [Docjure](https://github.com/mjul/docjure) 的库，该库提供对 Microsoft Office 文件格式的访问。
 
-;; Docjure's API is a bit low-level and doesn't make the obvious tasks
-;; easy, so we're going to use this helper function to make the code
-;; below clearer. Check out the line-by-line comments to see how this
-;; function works.
+;; Docjure 的 API 有点底层，并且没有使明显的任务变得容易，
+;; 因此我们将使用这个辅助函数使下面的代码更清晰。
+;; 查看逐行注释以了解此函数的工作原理。
 (defn load-first-sheet
-  "Return the first sheet of an Excel spreadsheet as a seq of maps."
+  "将 Excel 电子表格的第一个工作表作为 map 的 seq 返回。"
   [filename]
-  (let [rows (->> (ss/load-workbook filename) ; load the file
-                  (ss/sheet-seq)              ; seq of sheets in the file
-                  first                       ; take the first (only)
-                  ss/row-seq                  ; get the rows from it
-                  (mapv ss/cell-seq))         ; each row -> seq of cells
-        ;; break off the headers to produce a seq of maps
+  (let [rows (->> (ss/load-workbook filename) ; 加载文件
+                  (ss/sheet-seq)              ; 文件中工作表的 seq
+                  first                       ; 取第一个（也是唯一一个）
+                  ss/row-seq                  ; 从中获取行
+                  (mapv ss/cell-seq))         ; 每行 -> 单元格的 seq
+        ;; 分离标题以生成 map 的 seq
         headers   (mapv (comp keyword ss/read-cell) (first rows))]
-    ;; map over the rows creating new maps with the headers as keys
+    ;; 映射行，创建以标题作为键的新 map
     (mapv #(zipmap headers (map ss/read-cell %)) (rest rows))))
 
-;; Now we're going to use a few lines of code to:
-;; 1. Load the spreadsheet data.
-;; 2. Use `clojure.set`'s `join` function to combine our freshly
-;; loaded GINI spreadsheet with our previously prepared life
-;; expectancy data, which works because they are both sequences of
-;; maps that have a `:country` key.
-;; 3. Assoc a `:gini` key in each map to the World Bank's number, but
-;; falling back to the CIA's estimate. (These kinds of small
-;; programmatic tasks are a constant feature of data wrangling.)
+;; 现在，我们将使用几行代码来：
+;; 1. 加载电子表格数据。
+;; 2. 使用 `clojure.set` 的 `join` 函数将我们新加载的 GINI 电子表格与我们先前准备的预期寿命数据结合起来，
+;;    因为它们都是具有 `:country` 键的 map 的序列。
+;; 3. 在每个 map 中关联一个 `:gini` 键到世界银行的数字，但回退到 CIA 的估计值。
+;;    （这些类型的小型编程任务是数据整理的一个持续特征。）
 (def expectancy-and-gini
   (->> (load-first-sheet "datasets/countries-gini.xlsx")
        (join life-expectancy)
@@ -114,9 +94,8 @@
                 (assoc % :gini gini)
                 nil))))
 
-;; Expanding the Clojure data structures makes it look like this will
-;; work for our comparisons. Let's plot the data to see a list of
-;; countries from most to least equal:
+;; 展开 Clojure 数据结构使它看起来可以用于我们的比较。
+;; 让我们绘制数据图，以查看从最平等到最不平等的国家/地区的列表：
 (clerk/vl
  {:data {:values expectancy-and-gini}
   :width 600
@@ -129,8 +108,7 @@
                  :sort :x}
              :tooltip {:field :country}}})
 
-;; And now to have a look at whether inequality and life expectancy
-;; are correlated:
+;; 现在来看看不平等和预期寿命是否相关：
 (clerk/vl
  {:data {:values expectancy-and-gini}
   :mark :rect
@@ -145,55 +123,44 @@
              :color {:aggregate :count :type :quantitative}}
   :config {:view {:stroke :transparent}}})
 
-;; It seems like the mass of long lived countries are also in the
-;; lower two thirds of the inequality distribution. A little filtering
-;; shows is that the only really long-lived countries above a GINI
-;; coefficient of ~50 is Hong Kong.
-
+;; 看起来长寿国家的大部分也处于不平等分布的较低三分之二。
+;; 稍微过滤一下显示，唯一真正长寿的国家/地区的不平等系数高于 ~50 的是香港。
 (clerk/table
  (->> (filter #(< 50 (:gini %)) expectancy-and-gini)
       (sort-by :life-expectancy)))
 
-;; ## Happiness
+;; ## 幸福感
 
-;; Let's look at happiness! This time, we'll use
-;; [jdbc.next](https://github.com/seancorfield/next-jdbc) to perform a
-;; SQL query on a Sqlite data containing a table of countries and
-;; their relative happiness ratings. Note that we're changing the
-;; column name `:country_or_region` to `:country` using
-;; `clojure.set`'s `rename-keys` function so that this table will be
-;; easy to join with our others.
+;; 让我们来看看幸福感！这次，我们将使用 [jdbc.next](https://github.com/seancorfield/next-jdbc)
+;; 对 Sqlite 数据执行 SQL 查询，该数据包含一个国家/地区及其相对幸福度评分的表。
+;; 请注意，我们正在使用 `clojure.set` 的 `rename-keys` 函数将列名 `:country_or_region` 更改为 `:country`，
+;; 以便此表易于与我们的其他表连接。
 (def world-happiness
-  (let [_run-at #inst "2021-11-26T08:28:29.445-00:00" ; bump this to re-run the query!
+  (let [_run-at #inst "2021-11-26T08:28:29.445-00:00" ; 更改此值以重新运行查询！
         ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "./datasets/happiness.db"})]
     (->> (with-open [conn (jdbc/get-connection ds)]
            (jdbc/execute! conn ["SELECT * FROM happiness"]
                           {:return-keys true :builder-fn rs/as-unqualified-lower-maps}))
          (map #(rename-keys % {:country_or_region :country})))))
 
-;; Looking at the happiness data, it appears that all the usual
-;; suspects — Nordics, Western Europeans, Canadians, and Kiwis — are
-;; living pretty good lives by their own estimation. Looking closer,
-;; we see that although the top twenty countries all relatively
-;; prosperous, it's clear that GDP is not strongly correlated with
-;; happiness _within that cohort_.
-
+;; 查看幸福感数据，似乎所有常见的嫌疑人——北欧人、西欧人、加拿大人和新西兰人——
+;; 都根据自己的估计过着相当美好的生活。
+;; 仔细观察，我们发现虽然前 20 个国家/地区都相对富裕，但很明显 GDP 与_该队列内的_幸福感没有很强的相关性。
 (clerk/table world-happiness)
 
-;; Next, we're computing a linear regression for this dataset using [kixi.stats](https://github.com/MastodonC/kixi.stats).
+;; 接下来，我们使用 [kixi.stats](https://github.com/MastodonC/kixi.stats) 为此数据集计算线性回归。
 ^{::clerk/viewer {:transform-fn (clerk/update-val kixi-p/parameters)}}
 (def linear-regression
   (transduce identity (kixi-stats/simple-linear-regression :score :gdp) world-happiness))
 
-;; We'll use this linear regression to augment out dataset so each datapoint also gets a `:regression` value.
+;; 我们将使用此线性回归来扩充我们的数据集，以便每个数据点也获得一个 `:regression` 值。
 (def world-happiness+regression
   (mapv (fn [{:as datapoint :keys [score]}]
           (assoc datapoint :regression (kixi-p/measure linear-regression score)))
         world-happiness))
 
-;; Let's graph the relationship between happiness and GDP to get a
-;; bird's eye view on the situation over our entire dataset. You can
-;; mouse over individual data points to get more info:
+;; 让我们绘制幸福感和 GDP 之间的关系图，以从整体上了解情况。
+;; 您可以将鼠标悬停在单个数据点上以获取更多信息：
 
 ^{:nextjournal.clerk/visibility {:code :hide :result :show}}
 (clerk/vl
@@ -214,25 +181,15 @@
                       :y {:field :regression
                           :type :quantitative}}}]})
 
+;; 看起来，正如我们可能预料的那样，一般来说，富裕国家比贫穷国家更幸福，尽管存在差异和异常值。
+;; 例如，芬兰排名第一，但人均 GDP 与第 58 名日本相似。
+;; 也许更引人注目的是，卡塔尔的数据集中人均 GDP 最高，但卡塔尔人的平均幸福感与萨尔瓦多的人民大致相同。
+;; 同样，博茨瓦纳的人均 GDP 是马拉维的五倍，但它的人民并没有因此而更幸福。
+;; 如果我被迫猜测原因，我可能会推测，一个繁荣的国家，其所有财富都集中在极少数人手中，对于普通人来说仍然是一个相当糟糕的居住地。
 
-;; It looks, as we might have expected, like richer countries are
-;; happier than poor ones in general, though with variations and
-;; outliers. For example, Finland is in first place but has a similar
-;; GDP/capita as number 58, Japan. Perhaps even more striking, Qatar
-;; has the highest GDP/capita in the dataset, but Qataris are on
-;; average about as happy as people in El Salvador. Likewise, Botswana
-;; has five times the GDP/capita of Malawi, but its people are no
-;; happier for it. If I were forced to guess why, I might theorize
-;; that a properous country with all of its wealth concentrated in
-;; very few hands can still be a fairly wretched place to live for the
-;; average person to live.
-
-;; One way to investigate this possibility is to plot the correlation
-;; between equality and happiness in the rich world. We'll use `join`
-;; again, but we'll first use `clojure.set`'s `project` (named by
-;; analogy to SQL projection) to pluck just the `:country` and
-;; `:score` from the happiness dataset, then sort by the GDP and take
-;; the top 20 countries.
+;; 调查这种可能性的一种方法是绘制富裕世界中平等与幸福感之间的相关性。
+;; 我们将再次使用 `join`，但我们将首先使用 `clojure.set` 的 `project`（类似于 SQL 投影）
+;; 从幸福感数据集中仅提取 `:country` 和 `:score`，然后按 GDP 排序并取前 20 个国家/地区。
 (clerk/vl
  {:data {:values (->> (project world-happiness [:country :score])
                       (join expectancy-and-gini)
@@ -249,9 +206,6 @@
                  :scale {:zero false}}
              :tooltip {:field :country}}})
 
-;; This does, at least at first glance, support the notion that the
-;; happiest people — just like the longest lived ones — tend to
-;; inhabit countries in the more equal part of the GINI distribution.
+;; 至少乍一看，这确实支持了这样一种观点，即最幸福的人——就像寿命最长的人一样——倾向于居住在基尼分布中更平等的部分的国家。
 
-;; I hope this example gives you some ideas about things you'd like to
-;; investigate.
+;; 我希望这个例子能给你一些你想要调查的想法。
--- notebooks/sicmutils.clj
+++ notebooks/sicmutils.clj
@@ -1,13 +1,3 @@
-;; # Exercise 1.44: The double pendulum
-
-;; This namespace explores [Exercise
-;; 1.44](https://tgvaughan.github.io/sicm/chapter001.html#Exe_1-44) from Sussman
-;; and Wisdom's [Structure and Interpretation of Classical
-;; Mechanics](https://tgvaughan.github.io/sicm/), using
-;; the [SICMUtils](https://github.com/sicmutils/sicmutils) Clojure library and
-;; the Clerk rendering environment.
-
-#_{:clj-kondo/ignore [:refer-all]}
 (ns sicmutils
   (:refer-clojure
    :exclude [+ - * / partial ref zero? numerator denominator compare = run!])
@@ -15,12 +5,9 @@
             [sicmutils.env :as e :refer :all]
             [sicmutils.expression.render :as xr]))
 
-;; ## Lagrangian
+;; ## Lagrangian（拉格朗日量）
 ;;
-;; Start with a coordinate transformation from `theta1`, `theta2` to rectangular
-;; coordinates. We'll generate our Lagrangian by composing this with an rectangular
-;; Lagrangian with the familiar form of `T - V`.
-
+;; 从一个从 `theta1`, `theta2` 到矩形坐标的坐标变换开始。我们将通过将此变换与具有熟悉的 `T - V` 形式的矩形拉格朗日量组合来生成我们的拉格朗日量。
 (defn angles->rect [l1 l2]
   (fn [[_ [theta1 theta2]]]
     (let [x1 (* l1 (sin theta1))
@@ -29,9 +16,7 @@
           y2 (- y1 (* l2 (cos (+ theta1 theta2))))]
       (up x1 y1 x2 y2))))
 
-;; `T` describes the sum of the kinetic energy of two particles in rectangular
-;; coordinates.
-
+;; `T` 描述了两个粒子在矩形坐标系中的动能之和。
 (defn T [m1 m2]
   (fn [[_ _ [xdot1 ydot1 xdot2 ydot2]]]
     (+ (* 1/2 m1 (+ (square xdot1)
@@ -40,31 +25,27 @@
                     (square ydot2))))))
 
 
-;; `V` describes a uniform gravitational potential with coefficient `g`, acting
-;; on two particles with masses of, respectively, `m1` and `m2`. Again, this is
-;; written in rectangular coordinates.
+;; `V` 描述了一个均匀的引力势，系数为 `g`，作用于质量分别为 `m1` 和 `m2` 的两个粒子。同样，这是用矩形坐标写成的。
 
 (defn V [m1 m2 g]
   (fn [[_ [_ y1 _ y2]]]
     (+ (* m1 g y1)
        (* m2 g y2))))
 
-;; Form the rectangular Lagrangian `L` by subtracting `(V m1 m2 g)` from `(T m1 m2)`:
+;; 通过从 `(T m1 m2)` 中减去 `(V m1 m2 g)` 来形成矩形拉格朗日量 `L`：
 
 (defn L-rect [m1 m2 g]
   (- (T m1 m2)
      (V m1 m2 g)))
 
-;; Form the final Langrangian in generalized coordinates (the angles of each
-;; segment) by composing `L-rect` with a properly transformed `angles->rect`
-;; coordinate transform!
+;; 通过将 `L-rect` 与正确转换的 `angles->rect` 坐标变换组合，形成广义坐标（每个段的角度）中的最终拉格朗日量！
 
 (defn L-double-pendulum [m1 m2 l1 l2 g]
   (compose (L-rect m1 m2 g)
            (F->C
             (angles->rect l1 l2))))
 
-;; The Lagrangian is big and hairy:
+;; 拉格朗日量很大且复杂：
 
 (def symbolic-L
   ((L-double-pendulum 'm_1 'm_2 'l_1 'l_2 'g)
@@ -72,19 +53,18 @@
        (up 'theta_1 'theta_2)
        (up 'theta_1dot 'theta_2dot))))
 
-;; Let's simplify that:
+;; 让我们简化一下：
 
 (simplify symbolic-L)
 
-;; Better yet, let's render it as LaTeX, and create a helper function,
-;; `render-eq` to make it easier to render simplified equations:
+;; 更好的是，让我们将其渲染为 LaTeX，并创建一个辅助函数 `render-eq`，以便更容易渲染简化的方程：
 
 (def render-eq
   (comp clerk/tex ->TeX simplify))
 
 (render-eq symbolic-L)
 
-;; And here are the equations of motion for the system:
+;; 这是系统的运动方程：
 
 (let [L (L-double-pendulum 'm_1 'm_2 'l_1 'l_2 'g)]
   (binding [xr/*TeX-vertical-down-tuples* true]
@@ -94,60 +74,52 @@
            (literal-function 'theta_2)))
       't))))
 
-;; What do these mean?
+;; 这些意味着什么？
 ;;
-;; - the system has two degrees of freedom: $\theta_1$ and $\theta_2$.
-;; - at any point `t` in time, the two equations above, full of first and second
-;; - order derivatives of the position functions, will stay true
-;; - the system can use these equations to simulate the system, one tick at a time.
+;; - 系统有两个自由度：$\theta_1$ 和 $\theta_2$。
+;; - 在任何时间点 `t`，上述两个方程（充满了位置函数的一阶和二阶导数）将保持为真
+;; - 系统可以使用这些方程来一次一个刻度地模拟系统。
 
-;; ## Simulation
+;; ## Simulation（模拟）
 ;;
-;; Next, let's run a simulation using those equations of motion and collect data
-;; on each coordinate's evolution.
+;; 接下来，让我们使用这些运动方程运行一个模拟，并收集每个坐标演化的数据。
 ;;
-;; Here are the constants specified in exercise 1.44:
+;; 这是练习 1.44 中指定的常量：
 ;;
-;; masses in kg:
+;; 质量单位为 kg：
 
 (def m1 1.0)
 (def m2 3.0)
 
-;; lengths in meters:
+;; 长度单位为米：
 
 (def l1 1.0)
 (def l2 0.9)
 
-;; `g` in units of m/s^2:
+;; `g` 的单位为 m/s^2：
 
 (def g 9.8)
 
-;; And two sets of initial pairs of `theta1`, `theta2` angles corresponding to
-;; chaotic and regular initial conditions:
+;; 这是两组 `theta1`、`theta2` 角度的初始对，分别对应于混沌和规则的初始条件：
 
 (def chaotic-initial-q (up (/ Math/PI 2) Math/PI))
 (def regular-initial-q (up (/ Math/PI 2) 0.0))
 
-;; Composing `Lagrangian->state-derivative` with `L-double-pendulum` produces
-;; a state derivative that we can use with our ODE solver:
+;; 将 `Lagrangian->state-derivative` 与 `L-double-pendulum` 组合会产生一个状态导数，我们可以将其与 ODE 求解器一起使用：
 
 (def state-derivative
   (compose
    Lagrangian->state-derivative
    L-double-pendulum))
 
-;; Finally, two default parameters for our simulation. We'll record data in
-;; steps of 0.01 seconds, and simulate to a horizon of 50 seconds.
+;; 最后，我们模拟的两个默认参数。我们将以 0.01 秒的步长记录数据，并模拟到 50 秒的时限。
 
 (def step 0.01)
 (def horizon 50)
 
-;; `run!` will return a sequence of 5001 states, one for each measured point in
-;; the simulation. The smaller-arity version simply passes in default masses and
-;; lengths, but you can override those with the larger arity version if you like.
+;; `run!` 将返回一个包含 5001 个状态的序列，每个状态对应于模拟中每个测量点。较小arity的版本只传入默认的质量和长度，但如果需要，你可以用较大的arity版本覆盖这些值。
 
-;; (The interface here could use some work: `integrate-state-derivative` tidies
-;; this up a bit, but I want it out in the open for now.)
+;; （这里的接口可能需要改进：`integrate-state-derivative` 使其更整洁，但我现在想将其公开。）
 
 (defn run!
   ([step horizon initial-coords]
@@ -168,59 +140,50 @@
                    collector conj! state))})
      (persistent! @collector))))
 
-;; Run the simulation for each set of initial conditions and show the final
-;; state. Chaotic first:
+;; 为每组初始条件运行模拟，并显示最终状态。首先是混沌的：
 
 (def raw-chaotic-data
   (run! step horizon chaotic-initial-q))
 
-;; Looks good:
+;; 看起来不错：
 
 (peek raw-chaotic-data)
 
-;; Next, the regular initial condition:
+;; 接下来，是规则的初始条件：
 
 (def raw-regular-data
   (run! step horizon regular-initial-q))
 
-;; Peek at the final state:
+;; 查看最终状态：
 
 (peek raw-regular-data)
 
-;; ## Measurements, Data Transformation
+;; ## Measurements, Data Transformation（测量，数据转换）
 
-;; Next we'll chart the measurements trapped in those sequences of state tuples.
+;; 接下来，我们将绘制这些状态元组序列中捕获的测量值。
 
-;; The exercise asks us to graph the energy of the system as a function of time.
-;; Composing `Lagrangian->energy` with `L-double-pendulum` gives us a new
-;; function (of a state tuple!) that will return the current energy in the
-;; system.:
+;; 该练习要求我们将系统的能量绘制为时间的函数。将 `Lagrangian->energy` 与 `L-double-pendulum` 组合会产生一个新函数（状态元组的函数！），该函数将返回系统中的当前能量：
 
 (def L-energy
   (compose
    Lagrangian->energy
    L-double-pendulum))
 
-;; `energy-monitor` returns a function of `state` that stores an initial energy
-;; value in its closure, and returns the delta of each new state's energy as
-;; compared to the original.
+;; `energy-monitor` 返回一个 `state` 函数，该函数在其闭包中存储一个初始能量值，并返回每个新状态的能量与原始能量相比的增量。
 
 (defn energy-monitor [energy-fn initial-state]
   (let [initial-energy (energy-fn initial-state)]
     (fn [state]
       (- (energy-fn state) initial-energy))))
 
-;; Finally, the large `transform-data` function. The charting library we'll use
-;; likes Clojure dictionaries; `transform-data` turns our raw data into a
-;; sequence of dictionary with all values we might care to explore. This
-;; includes:
+;; 最后，是大型的 `transform-data` 函数。我们将使用的图表库喜欢 Clojure 字典；`transform-data` 将我们的原始数据转换为一个字典序列，其中包含我们可能关心的所有值。这包括：
 
-;; - the generalized coordinate angles
-;; - the generalized velocities of those angles
-;; - the rectangular coordinates of each pendulum bob
-;; - `:d-energy`, the error in energy at each point in the simulation
+;; - 广义坐标角度
+;; - 这些角度的广义速度
+;; - 每个钟摆摆锤的矩形坐标
+;; - `:d-energy`，模拟中每个点的能量误差
 
-;; Here is `transform-data`:
+;; 这是 `transform-data`：
 
 #_{:clj-kondo/ignore [:unresolved-symbol]}
 (defn transform-data [xs]
@@ -242,8 +205,7 @@
               :d-energy (monitor state)}))
          xs)))
 
-;; The following forms transform the raw data for each initial condition and
-;; bind the results to `chaotic-data` and `regular-data` for exploration.
+;; 以下形式转换每个初始条件的原始数据，并将结果绑定到 `chaotic-data` 和 `regular-data` 以进行探索。
 
 (def chaotic-data
   (doall
@@ -253,24 +215,21 @@
   (doall
    (transform-data raw-regular-data)))
 
-;; Here's the final, transformed chaotic state:
+;; 这是最终的，转换后的混沌状态：
 
 (last chaotic-data)
 
-;; And the similar regular state:
+;; 这是类似的规则状态：
 
 (last regular-data)
 
-;; ## Data Visualization Utilities
+;; ## Data Visualization Utilities（数据可视化实用程序）
 
-;; [Vega-Lite](https://vega.github.io/vega-lite/) allows us to
-;; visualizing the system.
+;; [Vega-Lite](https://vega.github.io/vega-lite/) 允许我们可视化系统。
 
-;; I am not a pro here, but this does the trick for now.
+;; 我不是这方面的专家，但现在这样做就可以了。
 
-;; First, a function to transform the dictionaries we generated above into a
-;; sequence of `x, y` coordinates tagged with distinct IDs for each pendulum
-;; bob's points:
+;; 首先，一个将我们上面生成的字典转换为 `x, y` 坐标序列的函数，并为每个钟摆摆锤的点标记不同的 ID：
 
 (defn points-data [data]
   (mapcat (fn [{:keys [t x1 y1 x2 y2]}]
@@ -284,7 +243,7 @@
               :id :p2}])
           data))
 
-;; `segments-data` generates endpoints of the pendulum segments, bob-to-bob:
+;; `segments-data` 生成钟摆段的端点，从摆锤到摆锤：
 
 (defn segments-data [data]
   (mapcat (fn [{:keys [t x1 y1 x2 y2]}]
@@ -302,21 +261,18 @@
               :id :p2}])
           data))
 
-;; ## Visualizations
+;; ## Visualizations（可视化）
 
-;; a helper function that should be in clojure.core
+;; 一个应该在 clojure.core 中的辅助函数
 (defn deep-merge [v & vs]
   (letfn [(rec-merge [v1 v2]
-                     (if (and (map? v1) (map? v2))
-                       (merge-with deep-merge v1 v2)
-                       v2))]
+            (if (and (map? v1) (map? v2))
+              (merge-with deep-merge v1 v2)
+              v2))]
     (when (some identity vs)
       (reduce #(rec-merge %1 %2) v vs))))
 
-;; With those tools in hand, let's make some charts. I'll call this first chart
-;; the `system-inspector`; this function will return a chart that will let us
-;; evolve the system with a time slider and monitor both angles, the energy
-;; error, and the pendulum bob itself as it evolves through time.
+;; 有了这些工具，让我们制作一些图表。我将第一个图表称为 `system-inspector`；此函数将返回一个图表，该图表将允许我们使用时间滑块来演化系统，并监视两个角度、能量误差以及钟摆摆锤本身随时间演化的过程。
 
 (defn system-inspector [data]
   {:config {:bar {:binSpacing 1, :discreteBandSize 5, :continuousBandSize 5}},
@@ -377,20 +333,19 @@
                               {:encoding {:y {:field :theta2},
                                           :tooltip [{:field :theta2}]}}])}]})
 
-;; Here's a system monitor for the chaotic initial condition:
+;; 这是混沌初始条件的系统监视器：
 
 (clerk/vl
  (system-inspector chaotic-data))
 
-;; And again for the regular initial condition:
+;; 再次是规则初始条件的系统监视器：
 
 (clerk/vl
  (system-inspector regular-data))
 
-;; ## Generalized coordinates, velocities
+;; ## Generalized coordinates, velocities（广义坐标，速度）
 
-;; `angles-plot` generates a plot, with no animation, showing both theta angles
-;; and their associated velocities:
+;; `angles-plot` 生成一个绘图，没有动画，显示了 theta 角度及其相关的速度：
 
 (defn angles-plot [data]
   (let [quarter-spec {:encoding {:y {:field :unassigned
@@ -422,26 +377,26 @@
                                 {:encoding {:y {:field :thetadot2}
                                             :tooltip [{:field :theta2}]}}])}]}))
 
-;; Here are the angles for the chaotic initial condition:
+;; 这是混沌初始条件的角度：
 
 (clerk/vl
  (angles-plot chaotic-data))
 
-;; And the regular initial condition:
+;; 这是规则初始条件的角度：
 
 (clerk/vl
  (angles-plot regular-data))
 
-;; ## Double Double Pendulum!
+;; ## Double Double Pendulum!（双重双摆！）
 
-;; No visualizations here yet, but the code works well.
+;; 这里还没有可视化，但是代码运行良好。
 
 (defn L-double-double-pendulum [m1 m2 l1 l2 g]
   (fn [[t [thetas1 thetas2] [qdots1 qdots2]]]
     (let [s1 (up t thetas1 qdots1)
-            s2 (up t thetas2 qdots2)]
-        (+ ((L-double-pendulum m1 m2 l1 l2 g) s1)
-           ((L-double-pendulum m1 m2 l1 l2 g) s2)))))
+          s2 (up t thetas2 qdots2)]
+      (+ ((L-double-pendulum m1 m2 l1 l2 g) s1)
+         ((L-double-pendulum m1 m2 l1 l2 g) s2)))))
 
 (def dd-state-derivative
   (compose
@@ -484,15 +439,15 @@
 (def raw-dd-chaotic-data
   (run-double-double! step horizon chaotic-initial-q))
 
-;; Looks good:
+;; 看起来不错：
 
 (peek raw-dd-chaotic-data)
 
-;; Next, the regular initial condition:
+;; 接下来，是规则的初始条件：
 
 (def raw-dd-regular-data
   (run-double-double! step horizon regular-initial-q))
 
-;; Peek at the final state:
+;; 查看最终状态：
 
 (peek raw-dd-regular-data)
